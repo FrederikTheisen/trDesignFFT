2772c09af97bed0c4c3065b7fab068f21738f918
Frederik Theisen <frederiktheisen@gmail.com>
2021-06-15 00:47:47 +0200

    updated

--- run_design.sh
+++ run_design.sh
@@ -7,23 +7,14 @@
     #create new folder named by datetime
     #put output in results folder if possible
 
-
-input="./parameters.txt"
-while IFS= read -r line
-do
-  echo "$line"
-done < "$input"
-
 #implement output path
 python design.py
 
-#read metrics file and pick best sequence(s)
+#python script writes sequences to output.txt
 
 #loop sequences
 input="./output.txt"
 while IFS= read -r line
 do
-  data = line.split(',')
-  if tonumber(data[0]) > x
-    python predict.py -path '/home/filename.npz' -seq data[1]
+    python predict.py -path '/home/filename.npz' -seq line
 done < "$input"
